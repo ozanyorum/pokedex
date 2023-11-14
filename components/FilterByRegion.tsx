82f05372ceb98f2c13236eef20d7acdfa94535c4
Ozan Oktay <109618158+ozanyorum@users.noreply.github.com>
2023-11-14 21:12:03 +0300

    Border adjustment

--- components/FilterByRegion.tsx
+++ components/FilterByRegion.tsx
@@ -25,7 +25,7 @@ export function FilterByRegion({ onRegionChange }: any) {
   return (
     <>
       <h2 className="text-xl text-center mt-8 mb-2">Filter by Region</h2>
-      <div className="flex flex-wrap text-l text-center mx-auto mb-8 p-4 justify-center gap-4 border border-gray-500">
+      <div className="flex flex-wrap text-l text-center mx-auto mb-8 p-4 justify-center gap-4 border border-y-1 border-muted">
         {regions.map((region) => (
           <h2
             key={region}
