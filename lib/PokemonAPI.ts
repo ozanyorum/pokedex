80ca964f78f6305b0c2236ec4155b4201abeee57
Ozan Oktay <109618158+ozanyorum@users.noreply.github.com>
2023-11-14 16:12:22 +0300

    updated API file

--- lib/PokemonAPI.ts
+++ lib/PokemonAPI.ts
@@ -1,13 +1,15 @@
 const POKEMON_API = "https://pokeapi.co/api/v2/";
 
-export async function getPokemonList() {
-    const response = await fetch(POKEMON_API + "pokemon?limit=151&offset=0");
-    const data = await response.json();
-    return data.results;
+export async function getPokemonList(limit: number, offset: number) {
+  const response = await fetch(
+    `${POKEMON_API}pokemon?limit=${limit}&offset=${offset}`
+  );
+  const data = await response.json();
+  return data.results;
 }
 
 export async function getPokemon(name: string) {
-    const response = await fetch(POKEMON_API + "pokemon/" + name);
-    const data = await response.json();
-    return data;
+  const response = await fetch(POKEMON_API + "pokemon/" + name);
+  const data = await response.json();
+  return data;
 }
